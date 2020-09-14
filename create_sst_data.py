bce1311923479c88381a516906547ee7478e0f56
Varun Lakshmanan <varunlakshmanan@outlook.com>
2020-09-13 23:59:49 -0400

    Pickled train, val, test

--- create_sst_data.py
+++ create_sst_data.py
@@ -15,15 +15,19 @@ pos = 1
 neg = 0
 seed = 42
 
-with open('data/stsa.binary.train.txt', 'r') as file:
+with open('data/sst2/stsa.binary.train.txt', 'r') as file:
     train_df = pd.read_csv(StringIO(''.join(line[0] + '\t' + line[2:] for line in file.readlines())), sep='\t')
 train_df.columns = ['sentiment', 'sentence']
-with open('data/stsa.binary.dev.txt', 'r') as file:
+with open('data/sst2/stsa.binary.dev.txt', 'r') as file:
     val_df = pd.read_csv(StringIO(''.join(line[0] + '\t' + line[2:] for line in file.readlines())), sep='\t')
 val_df.columns = ['sentiment', 'sentence']
-with open('data/stsa.binary.test.txt', 'r') as file:
+with open('data/sst2/stsa.binary.test.txt', 'r') as file:
     test_df = pd.read_csv(StringIO(''.join(line[0] + '\t' + line[2:] for line in file.readlines())), sep='\t')
 test_df.columns = ['sentiment', 'sentence']
 
 subsampled_train, throwaway = train_test_split(train_df, train_size=0.01, random_state=seed)
 subsampled_val, throwaway = train_test_split(val_df, train_size=(10/len(val_df)), random_state=seed, stratify=val_df['sentiment'])
+
+pickle.dump(subsampled_train, open('data/sst2/sst2_1_percent_train.pkl', 'wb'))
+pickle.dump(subsampled_val, open('data/sst2/sst2_10_samples_val.pkl', 'wb'))
+pickle.dump(test_df, open('data/sst2/sst2_all_test.pkl', 'wb'))
